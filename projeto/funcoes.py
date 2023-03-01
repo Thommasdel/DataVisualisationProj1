ac1ac6b15c23a67873341b870aa3effb0e4ebddc
Thomas Delfs <thomas.delfs2@hotmail.com>
2023-03-01 12:43:05 -0300

    Adição dos gráficos de barra e histograma

--- projeto/funcoes.py
+++ projeto/funcoes.py
@@ -15,5 +15,24 @@ def calcula_media(listagem): #A função calcula a média dos 3 maiores notas ob
     while pos < 3:
         soma += lista_ordenada[pos]
         pos += 1
-    return round(soma/3, 2) #Função round() arredonda os vaalores decimais
+    return round(soma/3, 4) #Função round() arredonda os vaalores decimais
+
+
+def pega_nota(dicionario, posicao_nota):
+    """Função que recebe um dicionário cujos valores são um iterável com as notas
+        dos alunos e recebe a posição da nota desejada (Maior nota, 2' maior nota, etc)
+    """
+
+    lista = []
+    valores = dicionario.values()
+    for i in valores:
+        maior_nota = ordena_notas(i)[posicao_nota]
+        lista.append(maior_nota)
+    return lista
+
+def legenda_automatica(grafico):
+    for i in grafico:
+        altura = i.get_height()
+        plt.annotate(f'{altura}', xy=(i.get_x() + i.get_width()/2, altura),
+                     xytext=(0, 2), textcoords="offset points", ha='center')
 
--- projeto/main.py
+++ projeto/main.py
@@ -32,4 +32,38 @@ for posicao in n_alunos:
 
 medias_alunos = {chave: funcoes.calcula_media(valor) for chave, valor in dados.items()}
 
-print(medias_alunos)
+# Gráfico de colunas mostrando as 3 maiores notas de cada aluno
+
+x = arange(len(n_alunos))
+largura = 0.3 # Largura das colunas
+
+notas_1 = funcoes.pega_nota(dados, 1)
+notas_2 = funcoes.pega_nota(dados, 2)
+notas_3 = funcoes.pega_nota(dados, 3)
+
+grafico1 = plt.bar(x - 3*largura/2, notas_1, width=largura, label="1ª Maior Nota", align="edge")
+grafico2 = plt.bar(x, notas_2, width=largura, label="2ª Maior Nota", align="center")
+grafico3 = plt.bar(x + largura/2, notas_3, width=largura, label="3ª Maior Nota", align='edge')
+
+plt.xlabel("Alunos")
+plt.title("Maiores notas por aluno")
+plt.xticks(x, dados.keys())
+plt.legend()
+
+funcoes.legenda_automatica(grafico1)
+funcoes.legenda_automatica(grafico2)
+funcoes.legenda_automatica(grafico3)
+
+
+plt.show()
+
+# Histograma com as médias finais
+
+plt.hist(medias_alunos.values(), bins=25, align='mid', histtype='barstacked', 
+        color='cyan', label='Médias finais' )
+
+plt.legend()
+plt.xlabel("Médias")
+plt.ylabel("Número de alunos")
+plt.title("Médias gerais")
+plt.show()
